0d4ffee54357d52f64b907a373d07b1fd84071e2
Yvan Ross <Yvan.ross@gmail.com>
2014-01-16 17:24:22 -0500

    non working textfield

---
+++ app/controllers/alert_view_temp.rb
@@ -0,0 +1,201 @@
+
+# class AlertsViewController < UITableViewController
+
+#   ALERT_CELL_ID = "AlertCellID"
+#   SOURCE_CELL_ID = "SourceCellID"
+
+#   UIACTION_SIMPLE_SECTION = 0
+#   UIACTION_OKCANCEL_SECTION = 1
+#   UIACTION_CUSTOM_SECTION = 2
+#   UIALERT_SIMPLE_SECTION = 3
+#   UIALERT_OKCANCEL_SECTION = 4
+#   UIALERT_CUSTOM_SECTION = 5
+#   UIALERT_SECURETEXT_SECTION = 6
+
+#   def viewDidLoad
+#     super
+#     self.title = "AlertTitle"
+ 
+#     @data_source_array = [
+#       { title: "UIActionSheet", label: "Show Simple", source: "AlertsViewController.m - dialogSimpleAction" },
+#       { title: "UIActionSheet", label: "Show OK-Cancel", source: "AlertsViewController.m - dialogOKCancelAction" },
+#       { title: "UIActionSheet", label: "Show Customized", source: "AlertsViewController.m - dialogOtherAction" },
+#       { title: "UIAlertView", label: "Show Simple", source: "AlertsViewController.m - alertSimpleAction" },
+#       { title: "UIAlertView", label: "Show OK-Cancel", source: "AlertsViewController.m - alertOKCancelAction" },
+#       { title: "UIAlertView", label: "Show Custom", source: "AlertsViewController.m - alertOtherAction" },
+#       { title: "UIAlertView", label: "Show Text Input", source: "AlertsViewController.m - alertSecureTextAction" }
+#     ]
+#   end
+
+# - (void)dialogSimpleAction
+# {
+#     // open a dialog with just an OK button
+#     UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:NSLocalizedString(@"UIActionSheetTitle", nil)
+#                                                              delegate:self
+#                                                     cancelButtonTitle:nil
+#                                                destructiveButtonTitle:NSLocalizedString(@"OKButtonTitle", nil)
+#                                                     otherButtonTitles:nil];
+#     actionSheet.actionSheetStyle = UIActionSheetStyleDefault;
+#     [actionSheet showInView:self.view]; // show from our table view (pops up in the middle of the table)
+# }
+ 
+
+#   def dialogSimpleAction
+#     action_sheet = UIActionSheet.alloc.initWithTitle("UIActionSheetTitle",
+#       delegate:self,
+#       cancelButtonTitle: nil,
+#       destructiveButtonTitle: "OKButtonTitle",
+#       otherButtonTitles: nil
+#     )
+#     action_sheet.actionSheetStyle = UIActionSheetStyleDefault
+#     action_sheet.showInView(self.view)
+#   end
+ 
+#   def dialogOKCancelAction
+#     action_sheet = UIActionSheet.alloc.initWithTitle("UIActionSheetTitle",
+#       delegate:self,
+#       cancelButtonTitle: "CancelButtonTitle",
+#       destructiveButtonTitle: "OKButtonTitle",
+#       otherButtonTitles: nil
+#     )
+#     action_sheet.actionSheetStyle = UIActionSheetStyleDefault
+#     action_sheet.showInView(self.view)
+#   end
+ 
+#   def dialogOtherAction
+#     action_sheet = UIActionSheet.alloc.initWithTitle("UIActionSheetTitle",
+#       delegate:self,
+#       cancelButtonTitle: nil,
+#       destructiveButtonTitle: nil,
+#       otherButtonTitles: "ButtonTitle1", "ButtonTitle2", nil
+#     )
+#     action_sheet.actionSheetStyle = UIActionSheetStyleDefault
+#     action_sheet.destructiveButtonIndex = 1
+#     action_sheet.showInView(self.view)
+#   end
+ 
+ 
+#   def alertSimpleAction
+#     alert = UIAlertView.alloc.initWithTitle("UIAlertViewTitle",
+#       message: "UIAlertViewMessageGeneric",
+#       delegate: self,
+#       cancelButtonTitle: "OKButtonTitle",
+#       otherButtonTitles: nil
+#     )
+#     alert.show
+#   end
+ 
+#   def alertOKCancelAction
+#     alert = UIAlertView.alloc.initWithTitle("UIAlertViewTitle",
+#       message: "UIAlertViewMessageGeneric",
+#       delegate: self,
+#       cancelButtonTitle: "CancelButtonTitle",
+#       otherButtonTitles: "OKButtonTitle", nil
+#     )
+#     alert.show
+#   end
+
+#   def alertOtherAction
+#     alert = UIAlertView.alloc.initWithTitle("UIAlertViewTitle",
+#       message: "UIAlertViewMessageGeneric",
+#       delegate: self,
+#       cancelButtonTitle: "CancelButtonTitle",
+#       otherButtonTitles: "ButtonTitle1", "ButtonTitle2", nil
+#     )
+#     alert.show
+#   end
+
+#   def alertSecureTextAction
+#     alert = UIAlertView.alloc.initWithTitle("UIAlertViewTitle",
+#       message: "UIAlertViewMessage",
+#       delegate: self,
+#       cancelButtonTitle: "CancelButtonTitle",
+#       otherButtonTitles: "OKButtonTitle", nil
+#     )
+#     alert.alertViewStyle = UIAlertViewStyleSecureTextInput
+#     alert.show
+#   end
+
+#   def actionSheet(action_sheet, clickedButtonAtIndex: button_index)
+#     if button_index == 0
+#       NSLog("ok")
+#     else
+#       NSLog("cancel")
+#     end
+#   end
+ 
+#   def alertView(action_sheet, clickedButtonAtIndex: button_index)
+#     NSLog("Clicked alert view at index: #{button_index}")
+#     # use "buttonIndex" to decide your action
+#   end
+ 
+#   def numberOfSectionsInTableView(table_view)
+#     @data_source_array.count
+#   end
+  
+#   def tableView(table_view, titleForHeaderInSection: section)
+#     @data_source_array[section][:title]
+#   end
+ 
+#   def tableView(table_view, numberOfRowsInSection: section)
+#     2
+#   end
+ 
+#   def tableView(table_view, heightForRowAtIndexPath: index_path)
+#     height = index_path.row == 0 ? 50.0 : 22.0
+#     p "Height: #{height}"
+#     return height
+#   end
+
+#   def tableView(table_view, didSelectRowAtIndexPath: index_path)
+#     tableView.deselectRowAtIndexPath(table_view.indexPathForSelectedRow, animated: true)
+
+#     if index_path.row == 0
+#       case index_path.section
+#       when UIACTION_SIMPLE_SECTION
+#         dialogSimpleAction
+#       when UIACTION_OKCANCEL_SECTION
+#         dialogOKCancelAction
+#       when UIACTION_CUSTOM_SECTION
+#         dialogOtherAction
+#       when UIALERT_SIMPLE_SECTION
+#         alertSimpleAction
+#       when UIALERT_OKCANCEL_SECTION
+#         alertOKCancelAction
+#       when UIALERT_CUSTOM_SECTION
+#         alertOtherAction
+#       when UIALERT_SECURETEXT_SECTION
+#         alertSecureTextAction
+#       end
+#     end
+#   end
+
+#   def tableView(table_view, cellForRowAtIndexPath: index_path)
+#     cell = nil
+
+#     if index_path.row == 0
+#       cell = tableView.dequeueReusableCellWithIdentifier(ALERT_CELL_ID) || begin
+#         UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: ALERT_CELL_ID)
+#       end
+#       cell.textLabel.text = @data_source_array[index_path.section][:label]
+#     else
+
+#       cell = tableView.dequeueReusableCellWithIdentifier(SOURCE_CELL_ID) || begin
+#         UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: SOURCE_CELL_ID)
+#       end
+#       cell.selectionStyle = UITableViewCellSelectionStyleNone
+        
+#       cell.textLabel.opaque = false
+#       cell.textLabel.textAlignment = NSTextAlignmentCenter
+#       cell.textLabel.textColor = UIColor.grayColor
+#       cell.textLabel.numberOfLines = 2
+#       cell.textLabel.font = UIFont.systemFontOfSize(12)
+      
+#       cell.textLabel.text = @data_source_array[index_path.section][:source]
+#     end
+
+#     cell
+
+#   end
+
+# end
---
+++ app/controllers/text_field_controller.rb
@@ -0,0 +1,208 @@
+class TextFieldController < UITableViewController
+
+  TEXT_FIELD_WIDTH = 260.0
+  TEXT_FIELD_CELL_ID = "TextFieldCellID"
+  SOURCE_CELL_ID = "SourceCellID"
+  VIEW_TAG = 1
+
+  def viewDidLoad
+    super
+    self.title = "TextFieldTitle"
+
+    @data_source_array = [
+      {  title: "UITextField",            source: "TextFieldController.m: textFieldNormal",   view: textFieldNormal},
+      {  title: "UITextField Rounded",    source: "TextFieldController.m: textFieldRounded",  view: textFieldRounded},
+      {  title: "UITextField Secure",     source: "TextFieldController.m: textFieldSecure",   view: textFieldSecure},
+      {  title: "UITextField Left View",  source: "TextFieldController.m: textFieldLeftView", view: textFieldLeftView}
+    ]
+
+    # we aren't editing any fields yet, it will be in edit when the user touches an edit field
+    @editing = false
+
+    # register our cell IDs for later when we are asked for UITableViewCells
+    # [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:TEXT_FIELD_CELL_ID]
+    # ableViewCell class] forCellReuseIdentifierSOURCE_CELL_ID
+  end
+
+
+
+  def numberOfSectionsInTableView
+    @data_source_array.count
+  end
+
+  def tableView(tableView, titleForHeaderInSection:section)
+    @data_source_array[section][:title]
+  end
+
+  def tableView(tableView, numberOfRowsInSection:section)
+    2
+  end
+
+  def tableView(tableView, heightForRowAtIndexPath:index_path)
+    height = index_path.row == 0 ? 50.0 : 22.0
+    p "Height: #{height}"
+    return height
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath:index_path)
+
+    cell = nil
+
+    if index_path.row == 0
+      cell = tableView.dequeueReusableCellWithIdentifier(TEXT_FIELD_CELL_ID,forIndexPath:index_path)
+      cell.selectionStyle = UITableViewCellSelectionStyleNone
+
+      viewToCheck = nil
+      cell.contentView(viewWithTag:VIEW_TAG)
+      if viewToCheck
+        viewToCheck.removeFromSuperview
+
+        textField = @data_source_array[index_path.section][:view]
+        
+        CGRect newFrame = textField.frame # make sure this textfield's width matches the width of the cell
+        newFrame.size.width = CGRectGetWidth(cell.contentView.frame) - kLeftMargin*2
+        textField.frame = newFrame
+        textField.autoresizingMask = UIViewAutoresizingFlexibleWidth # if the cell is ever resized, keep the textfield's width to match the cell's width
+
+        cell.contentView.addSubview(textField)
+      else
+        cell = tableView.dequeueReusableCellWithIdentifier(SOURCE_CELL_ID, index_path) || begin
+          UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:SOURCE_CELL_ID)
+        end
+
+        cell.selectionStyle = UITableViewCellSelectionStyleNone
+        cell.textLabel.textAlignment = NSTextAlignmentCenter
+        cell.textLabel.textColor = UIColor.grayColor
+        cell.textLabel.highlightedTextColor = UIColor.blackColor
+        cell.textLabel.font = UIFont.systemFontOfSize(12.0)
+
+        cell.textLabel.text = @data_source_array[index_path.section][:source]
+      end
+
+      return cell
+    end
+  end
+
+
+  def textFieldShouldReturn(textField)
+    textField.resignFirstResponder # the user pressed the "Done" button, so dismiss the keyboard
+    true
+  end
+
+  def textFieldNormal
+    if @text_field_normal.nil?     
+      frame = CGRectMake(kLeftMargin, 8.0, TEXT_FIELD_WIDTH, kTextFieldHeight)
+      @text_field_normal = UITextField.alloc.initWithFrame(frame)
+
+      @text_field_normal.borderStyle = UITextBorderStyleBezel
+      @text_field_normal.textColor = UIColor.blackColor
+      @text_field_normal.font = UIFont.systemFontOfSize(17.0)
+      @text_field_normal.placeholder = "<enter text>"
+      @text_field_normal.backgroundColor = UIColor.whiteColor
+      @text_field_normal.autocorrectionType = UITextAutocorrectionTypeNo    # no auto correction support
+
+      @text_field_normal.keyboardType = UIKeyboardTypeDefault               # use the default type input method (entire keyboard)
+      @text_field_normal.returnKeyType = UIReturnKeyDone
+
+      @text_field_normal.clearButtonMode = UITextFieldViewModeWhileEditing  # has a clear 'x' button to the right
+
+      @text_field_normal.tag = VIEW_TAG                                     # tag this control so we can remove it later for recycled cell
+      @text_field_normal.delegate = self                                    # let us be the delegate so we know when the keyboard's "Done" button is pressed
+
+      @text_field_normal.setAccessibilityLabel = "NormalTextField"
+    end 
+    return @text_field_normal
+  end
+
+  def textFieldRounded
+    if @text_field_rounded.nil?
+      frame = CGRectMake(kLeftMargin, 8.0, TEXT_FIELD_WIDTH, kTextFieldHeight)
+      @text_field_rounded = UITextField.alloc.initWithFrame(frame)
+
+      @text_field_rounded.borderStyle = UITextBorderStyleRoundedRect
+      @text_field_rounded.textColor = UIColor.blackColor
+      @text_field_rounded.font = UIFont.systemFontOfSize(17.0)
+      @text_field_rounded.placeholder = "<enter text>"
+      @text_field_rounded.backgroundColor = UIColor.whiteColor
+
+      @text_field_rounded.autocorrectionType = UITextAutocorrectionTypeNo  # no auto correction support
+
+      @text_field_rounded.keyboardType = UIKeyboardTypeDefault
+      @text_field_rounded.returnKeyType = UIReturnKeyDone
+
+      @text_field_rounded.clearButtonMode = UITextFieldViewModeWhileEditing    # has a clear 'x' button to the right
+
+      @text_field_rounded.tag = VIEW_TAG       # tag this control so we can remove it later for recycled cell
+
+      @text_field_rounded.delegate = self  # let us be the delegate so we know when the keyboard's "Done" button is pressed
+
+      @text_field_rounded.setAccessibilityLabel("RoundedTextField")
+    end
+    return @text_field_rounded
+  end
+
+  def textFieldSecure
+    if (@text_field_secure == nil)
+      frame = CGRectMake(kLeftMargin, 8.0, TEXT_FIELD_WIDTH, kTextFieldHeight)
+      @text_field_secure = UITextField.alloc.initWithFrame(frame)
+
+      @text_field_secure.borderStyle = UITextBorderStyleBezel
+      @text_field_secure.textColor = UIColor.blackColor
+      @text_field_secure.font = UIFont.systemFontOfSize(17.0)
+      @text_field_secure.placeholder = "<enter password>"
+      @text_field_secure.backgroundColor = UIColor.whiteColor
+
+      @text_field_secure.keyboardType = UIKeyboardTypeDefault
+      @text_field_secure.returnKeyType = UIReturnKeyDone   
+
+      @text_field_secure.secureTextEntry = true # make the text entry secure (bullets)
+
+      @text_field_secure.clearButtonMode = UITextFieldViewModeWhileEditing # has a clear 'x' button to the right
+
+      @text_field_secure.tag = VIEW_TAG        # tag this control so we can remove it later for recycled cell
+
+      @text_field_secure.delegate = self   # let us be the delegate so we know when the keyboard's "Done" button is pressed
+
+      @text_field_secure.setAccessibilityLabel("SecureTextField")
+    end
+    return @text_field_secure
+  end
+
+  def textFieldLeftView
+    if @text_field_left_view.nil?
+      frame = CGRectMake(kLeftMargin, 8.0, TEXT_FIELD_WIDTH, kTextFieldHeight)
+      @text_field_left_view = UITextField.alloc.initWithFrame(frame)
+      @text_field_left_view.borderStyle = UITextBorderStyleBezel
+      @text_field_left_view.textColor = UIColor.blackColor
+      @text_field_left_view.font = UIFont.systemFontOfSize(17.0)
+      @text_field_left_view.placeholder = "<enter text>"
+      @text_field_left_view.backgroundColor = UIColor.whiteColor
+
+      @text_field_left_view.keyboardType = UIKeyboardTypeDefault
+      @text_field_left_view.returnKeyType = UIReturnKeyDone 
+
+      @text_field_left_view.clearButtonMode = UITextFieldViewModeWhileEditing   # has a clear 'x' button to the right
+
+      @text_field_left_view.tag = VIEW_TAG      # tag this control so we can remove it later for recycled cell
+
+      # Add an accessibility label that describes the text field.
+      @text_field_left_view.setAccessibilityLabel("CheckMarkIcon")
+
+      UIImageView *image = UIImageView.alloc.initWithImage(imageNamed:"segment_check.png")
+      @text_field_left_view.leftView = image
+      @text_field_left_view.leftViewMode = UITextFieldViewModeAlways
+
+      @text_field_left_view.delegate = self # let us be the delegate so we know when the keyboard's "Done" button is pressed
+    end
+    return @text_field_left_view
+  end
+end
+
+
+
+
+
+
+
+
+
