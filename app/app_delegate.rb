2ed5b8d4f831b20a17e04d3aeca6a7ac71eb147c
Dennis Major <dennismajor1@gmail.com>
2014-01-16 22:54:31 -0500

    after merge of Alerts

---
+++ app/app_delegate.rb
@@ -0,0 +1,9 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = AlertsViewController.alloc.initWithStyle(UITableViewStyleGrouped)
+    @window.rootViewController.wantsFullScreenLayout = true
+    @window.makeKeyAndVisible
+    true
+  end
+end
---
+++ spec/controllers/alerts_view_controller_spec.rb
@@ -0,0 +1,25 @@
+describe 'AlertsViewController' do
+  tests AlertsViewController
+
+  def controller
+    rotate_device to: :portrait, button: :bottom
+    @alerts_view_controller = AlertsViewController.alloc.initWithStyle(UITableViewStyleGrouped)
+  end
+
+  after do
+    @alerts_view_controller = nil
+  end
+
+  it 'sets the title' do
+    @alerts_view_controller.title.should == 'AlertTitle'
+  end
+
+  it 'should have 7 rows' do
+    @alerts_view_controller.instance_variable_get('@data_source_array').count.should == 7
+  end
+
+  it 'should alert' do
+    view('Show Simple').should.not == nil
+  end
+
+end
