9b87a90e9572982a2f589df072e58f8ddba16cda
Dennis Major <dennismajor1@gmail.com>
2014-01-16 23:25:23 -0500

    removed a redundant app_delegate.rb - remaining app_delegate.rb located in delegate folder

--- app/app_delegate.rb
+++
@@ -1,9 +0,0 @@
-class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = AlertsViewController.alloc.initWithStyle(UITableViewStyleGrouped)
-    @window.rootViewController.wantsFullScreenLayout = true
-    @window.makeKeyAndVisible
-    true
-  end
-end
