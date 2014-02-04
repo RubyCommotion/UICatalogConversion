7b4ac61928c4b2742adbcd1ab8dd5d1082a88c49
Dennis Major <dennismajor1@gmail.com>
2014-02-04 18:50:14 -0500

    Changed rc_custom_picker_data_source code to fix rending problem.
    While the custom picker code ran - the rows were mis-aligned and not
    displayed in the window of the picker wheel.

--- app/delegate/rc_custom_picker_data_source.rb
+++ app/delegate/rc_custom_picker_data_source.rb
@@ -5,13 +5,17 @@ class RcCustomPickerDataSource < NSObject
     # use predetermined frame size
     super
     # create the data source for this custom picker
-    @custom_picker_array = []
+    #@custom_picker_array = []
+    #
+    #@custom_picker_array << RcCustomView.alloc.initWithTitle('Early Morning', image: UIImage.imageNamed('/images/12-6AM.png'))
+    #@custom_picker_array << RcCustomView.alloc.initWithTitle('Late Morning', image: UIImage.imageNamed('/images/6-12AM.png'))
+    #@custom_picker_array << RcCustomView.alloc.initWithTitle('Afternoon', image: UIImage.imageNamed('/images/12-6PM.png'))
+    #@custom_picker_array << RcCustomView.alloc.initWithTitle('Evening', image: UIImage.imageNamed('/images/6-12PM.png'))
     
-    @custom_picker_array << RcCustomView.alloc.initWithTitle('Early Morning', image: UIImage.imageNamed('/images/12-6AM.png'))
-    @custom_picker_array << RcCustomView.alloc.initWithTitle('Late Morning', image: UIImage.imageNamed('/images/6-12AM.png'))
-    @custom_picker_array << RcCustomView.alloc.initWithTitle('Afternoon', image: UIImage.imageNamed('/images/12-6PM.png'))
-    @custom_picker_array << RcCustomView.alloc.initWithTitle('Evening', image: UIImage.imageNamed('/images/6-12PM.png'))
-    
+    # two assignment lines of code added below are to fix a custom picker mis-alignment issue using the original UICatalog code for iOS 6
+    @time_of_day_titles =  %w(Early\ Morning Late\ Morning Afternoon Evening)
+    @image_named = [ UIImage.imageNamed('/images/12-6AM.png'), UIImage.imageNamed('/images/6-12AM.png'),
+                    UIImage.imageNamed('/images/12-6PM.png'), UIImage.imageNamed('/images/6-12PM.png') ]
     self
   end
   
@@ -27,7 +31,8 @@ class RcCustomPickerDataSource < NSObject
   end
   
   def pickerView(picker_view, numberOfRowsInComponent: component)
-    @custom_picker_array.count
+    #@custom_picker_array.count
+    @time_of_day_titles.count
   end
   
   def numberOfComponentsInPickerView(picker_view)
@@ -38,6 +43,8 @@ class RcCustomPickerDataSource < NSObject
   
   # tell the picker which view to use for a given component and row, we have an array of views to show
   def pickerView(picker_view, viewForRow: row, forComponent: component, reusingView: view)
-    @custom_picker_array[row]
+    # The call directly below causes mis-alignment issues with the pickers wheel window and its rows - IOS 7
+    #@custom_picker_array[row]
+    RcCustomView.alloc.initWithTitle(@time_of_day_titles[row], image: @image_named[row])
   end
 end
